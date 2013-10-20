1692807134863470170cc955b5fda6fd14484e76
Kevin Durfee <kevdurfee@gmail.com>
2013-10-19 21:48:42 -0700

    parser v1.0

--- proj_part1/skeleton_parser.py
+++ proj_part1/skeleton_parser.py
@@ -33,7 +33,7 @@ http://docs.python.org/library/xml.dom.html
 
 Happy parsing!
 """
-
+import os
 import sys
 from xml.dom.minidom import parse
 from re import sub
@@ -127,6 +127,170 @@ def transformDollar(money):
         return money
     return sub(r'[^\d.]', '', money)
 
+def printXmlNode(node):
+    print node.toxml()
+
+def write_element(element, outfile):
+    
+    if "" == element:
+        outfile.write("NULL")
+    else:
+        outfile.write(element)
+
+def printArrayToFile(array, outfile):
+    
+    i=0
+    for element in array:
+        print element 
+        if 0==i:
+            write_element(element, outfile)
+            i=1
+        else:
+            outfile.write(columnSeparator)
+            write_element(element, outfile)
+    outfile.write("\n")
+
+def printUser(user_array, outfile, debug):
+    if debug:
+        print "Printing User To File..."
+        print "UID: ", user_array[0]
+        print "Rating: ", user_array[1]
+        print "Location: ", user_array[2]
+        print "Country: ", user_array[3]
+    
+    printArrayToFile(user_array, outfile)
+
+def parseUser(seller_xml,seller_loc,seller_country, debug):
+    seller_uid = seller_xml.getAttributeNode("UserID").nodeValue
+    seller_rating = seller_xml.getAttributeNode("Rating").nodeValue
+
+    if debug:
+        print "Seller/Buyer UID: ", seller_uid
+        print "Seller/Buyer Rating: ", seller_rating
+        print "Seller/Buyer Location: ", seller_loc
+        print "Seller/Buyer Country: ", seller_country
+
+    seller_array = [ seller_uid, seller_rating, seller_loc, seller_country]
+
+    return seller_array
+
+def parseTime(item_xml, out_file, debug):
+    if debug:
+        print "Parsing Time..."
+
+    item_id = item_xml.getAttributeNode("ItemID").nodeValue
+    started = getElementTextByTagNameNR(item_xml, "Started")
+    ends = getElementTextByTagNameNR(item_xml, "Ends")
+
+    started = transformDttm(started)
+    ends = transformDttm(ends)
+
+    time_array = [item_id, started, ends]
+
+    printArrayToFile(time_array, out_file)
+
+def parseItem(item_xml,seller_id, out_file, debug):
+    if debug:
+        print "Parsing Item..."
+
+    item_id = item_xml.getAttributeNode("ItemID").nodeValue
+    name = getElementTextByTagNameNR(item_xml, "Name")
+    description = getElementTextByTagNameNR(item_xml, "Description")
+    num_of_bids = getElementTextByTagNameNR(item_xml, "Number_Of_Bids")
+
+    if debug:
+        print "item_id: ", item_id
+        print "name: ", name
+        print "description: ", description
+        print "num_of_bids: ", num_of_bids
+
+    item_array = [item_id,seller_id, name, description, num_of_bids]
+    printArrayToFile(item_array, out_file)    
+
+def parseItemRelations(item, out_file_dic, debug):
+    
+    seller_array = parseUsers(item,out_file_dic, debug)
+   
+    parseCat(item,out_file_dic["cat"], debug)
+
+    parsePrice(item, out_file_dic["price"], debug)
+
+    parseTime(item, out_file_dic["time"], debug)
+
+    parseItem(item,seller_array[0], out_file_dic["item"], debug)
+
+
+#def parseBid(item_xml, out_file, debug)
+
+def parsePrice(item_xml, out_file, debug):
+    if debug:
+        "Parsing Price..."
+
+    item_id = item_xml.getAttributeNode("ItemID").nodeValue
+    first_bid = getElementTextByTagNameNR(item_xml, "First_Bid")  
+    buy_price = getElementTextByTagNameNR(item_xml, "Buy_Price")
+    currently = getElementTextByTagNameNR(item_xml, "Currently")
+
+    first_bid = transformDollar(first_bid)
+    buy_price = transformDollar(buy_price)
+    currently = transformDollar(currently)
+
+    price_array = [item_id, first_bid, buy_price, currently]
+
+    printArrayToFile(price_array, out_file)
+
+def parseCat(item_xml, out_file, debug):
+    if debug:
+        "Parsing Category..."
+   
+    cats_xml_array = getElementsByTagNameNR(item_xml, "Category")
+    item_id = item_xml.getAttributeNode("ItemID").nodeValue
+    for cat_xml in cats_xml_array:
+        cat = getElementText(cat_xml)
+        cat_array = [item_id, cat]
+
+        printArrayToFile(cat_array,out_file)
+
+def parseUsers(item, out_file_dic, debug):
+    if debug:
+        print "Parsing Users" 
+
+    seller_array=parseUser(getElementByTagNameNR(item, "Seller"), \
+                                              getElementTextByTagNameNR(item, "Location"), \
+                                              getElementTextByTagNameNR(item, "Country"), debug)
+
+    bidder_array2 = []
+    bids_xml = getElementByTagNameNR(item, "Bids")
+    bid_xml_array = getElementsByTagNameNR(bids_xml, "Bid")
+    for bid_xml in bid_xml_array:
+        bidder_xml = getElementByTagNameNR(bid_xml, "Bidder")
+        bidder_array=parseUser(bidder_xml , \
+                           getElementTextByTagNameNR(bidder_xml, "Location"), \
+                           getElementTextByTagNameNR(bidder_xml, "Country"), debug)
+        parseBid(item,bid_xml, bidder_xml, out_file_dic["bid"], debug)
+        bidder_array2.append(bidder_array)
+
+
+    bidder_array2.append(seller_array)
+    user_array2 = bidder_array2
+    for user_array in user_array2:
+       printUser(user_array, out_file_dic["user"],debug)
+
+    return seller_array
+
+def parseBid(item_xml,bid_xml, bidder_xml, out_file, debug):
+    if debug:
+        print "Parsing Bid..."
+    
+    item_id = item_xml.getAttributeNode("ItemID").nodeValue
+    bidder_id = bidder_xml.getAttributeNode("UserID").nodeValue
+    time = getElementTextByTagNameNR(bid_xml, "Time")
+    amount = getElementTextByTagNameNR(bid_xml, "Amount")
+
+    bid_array = [item_id, bidder_id, time, amount]
+
+    printArrayToFile(bid_array, out_file)
+    
 """
 Parses a single xml file. Currently, there's a loop that shows how to parse
 item elements. Your job is to mirror this functionality to create all of the necessary SQL tables
@@ -136,6 +300,46 @@ def parseXml(f):
     """
     TO DO: traverse the dom tree to extract information for your SQL tables
     """
+    out_file_dic = {}
+    
+    out_file_n = os.path.basename(f)
+    out_file_n = out_file_n[0:-4]+"-SQLBULK"
+    #print "Creating User  Output File: ", out_file_n
+    user_file_n = out_file_n+"_USERS.dat"
+    user_file = open(user_file_n, "w")
+    out_file_dic["user"]=user_file
+
+    cat_file_n = out_file_n+"_CAT.dat"
+    out_file_dic["cat"] = open(cat_file_n, "w")
+
+    price_file_n = out_file_n+"_PRICE.dat"
+    out_file_dic["price"] = open(price_file_n, "w")
+
+    time_file_n = out_file_n+"_TIME.dat"
+    out_file_dic["time"] = open(time_file_n, "w")
+
+    item_file_n = out_file_n+"_ITEM.dat"
+    out_file_dic["item"] = open(item_file_n, "w")
+
+    bid_file_n = out_file_n+"_BID.dat"
+    out_file_dic["bid"] = open(bid_file_n, "w")
+
+    print "Parsing XML Document..."
+
+    print "Getting Items "
+    #items = printXmlNode(dom.childNodes[1])
+    items_xml = getElementByTagNameNR(dom, "Items")
+
+    print "Getting Individual Items"
+    item_xml_array = getElementsByTagNameNR(items_xml, "Item")
+
+    print "Parsing Item Array"
+    
+    debug =  6
+    for item in item_xml_array:
+        parseItemRelations(item, out_file_dic, 0)
+        debug = debug + 1
+
 
 """
 Loops through each xml files provided on the command line and passes each file
